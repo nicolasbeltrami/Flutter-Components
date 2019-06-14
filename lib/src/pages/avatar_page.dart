54dbc862f3604c77fe7c71b0e0a4eebcc38d0487
nicolasbeltrami <nicolas.beltrami@gmail.com>
2019-06-13 23:49:06 -0400

    avatar page finished

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -6,7 +6,30 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.all(3.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage('https://cdn.shopify.com/s/files/1/1511/4266/products/Fashion-Glasses-Small-Pet-Dogs-Cat-Glasses-Sunglasses-Eye-wear-Protection-Pet-Cool-Glasses-Pet-Photos.jpg?v=1521388987'),
+              radius: 29.0,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text('NB'),
+              backgroundColor: Colors.white,
+            ),
+          ),
+        ],
       ),
+      body: Center(
+        child: FadeInImage(
+          image: NetworkImage('https://cdn.shopify.com/s/files/1/1511/4266/products/Fashion-Glasses-Small-Pet-Dogs-Cat-Glasses-Sunglasses-Eye-wear-Protection-Pet-Cool-Glasses-Pet-Photos.jpg?v=1521388987'),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          fadeInDuration: Duration(microseconds: 200),
+        ) ,
+        ),
     );
   }
 }
